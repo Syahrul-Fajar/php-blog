1fe120d40a1aba25af186ebb3b1ef76ba7c4f78b
Syahrul-Fajar <syhrlfajar@gmail.com>
2025-05-16 01:56:18 +0700

    Update koneksi database Railway

--- config/db.php
+++ config/db.php
@@ -1,14 +1,12 @@
 <?php
-$host = getenv("MYSQLHOST") ?: "containers-us-west-123.railway.app";
-$port = getenv("MYSQLPORT") ?: 3306;
-$user = getenv("MYSQLUSER") ?: "root";
-$pass = getenv("MYSQL_ROOT_PASSWORD") ?: "PabwqmFOeawHLuZkCEWHSJdCveLGDfnh";
-$db   = getenv("MYSQLDATABASE") ?: "railway";
+$host = getenv("MYSQLHOST");
+$port = getenv("MYSQLPORT");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQL_ROOT_PASSWORD");
+$db   = getenv("MYSQLDATABASE");
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    var_dump($host, $port, $user, $pass, $db);
-    exit;
-    die("Koneksi gagal: " . $conn->connect_error);
-}
+echo "<pre>";
+var_dump($host, $port, $user, $pass, $db);
+echo "</pre>";
+exit;
+?>
