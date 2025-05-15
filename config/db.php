9f07da0235fc3ef69570d0583de97be822128f6f
Syahrul-Fajar <syhrlfajar@gmail.com>
2025-05-16 02:06:18 +0700

    Fix koneksi database dengan env variables

--- config/db.php
+++ config/db.php
@@ -1,12 +1,13 @@
 <?php
-$host = getenv('MYSQLHOST') ?: ($_ENV['MYSQLHOST'] ?? ($_SERVER['MYSQLHOST'] ?? 'containers-us-west-123.railway.app'));
-$port = getenv('MYSQLPORT') ?: ($_ENV['MYSQLPORT'] ?? ($_SERVER['MYSQLPORT'] ?? 3306));
-$user = getenv('MYSQLUSER') ?: ($_ENV['MYSQLUSER'] ?? ($_SERVER['MYSQLUSER'] ?? 'root'));
-$pass = getenv('MYSQL_ROOT_PASSWORD') ?: ($_ENV['MYSQL_ROOT_PASSWORD'] ?? ($_SERVER['MYSQL_ROOT_PASSWORD'] ?? 'your_password'));
-$db   = getenv('MYSQLDATABASE') ?: ($_ENV['MYSQLDATABASE'] ?? ($_SERVER['MYSQLDATABASE'] ?? 'railway'));
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQL_ROOT_PASSWORD');
+$db   = getenv('MYSQLDATABASE');
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+?>
