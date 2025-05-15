7d4c4e465255d4169d33fcccdb940dffa1dec242
Syahrul-Fajar <syhrlfajar@gmail.com>
2025-05-16 01:59:51 +0700

    Update koneksi database Railway

--- config/db.php
+++ config/db.php
@@ -1,12 +1,12 @@
 <?php
-$host = getenv("MYSQLHOST");
-$port = getenv("MYSQLPORT");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQL_ROOT_PASSWORD");
-$db   = getenv("MYSQLDATABASE");
+$host = getenv('MYSQLHOST') ?: ($_ENV['MYSQLHOST'] ?? ($_SERVER['MYSQLHOST'] ?? 'containers-us-west-123.railway.app'));
+$port = getenv('MYSQLPORT') ?: ($_ENV['MYSQLPORT'] ?? ($_SERVER['MYSQLPORT'] ?? 3306));
+$user = getenv('MYSQLUSER') ?: ($_ENV['MYSQLUSER'] ?? ($_SERVER['MYSQLUSER'] ?? 'root'));
+$pass = getenv('MYSQL_ROOT_PASSWORD') ?: ($_ENV['MYSQL_ROOT_PASSWORD'] ?? ($_SERVER['MYSQL_ROOT_PASSWORD'] ?? 'your_password'));
+$db   = getenv('MYSQLDATABASE') ?: ($_ENV['MYSQLDATABASE'] ?? ($_SERVER['MYSQLDATABASE'] ?? 'railway'));
 
-echo "<pre>";
-var_dump($host, $port, $user, $pass, $db);
-echo "</pre>";
-exit;
-?>
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
